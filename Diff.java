2d73a951d2d36308fa6f761f98685db80006694b
Akshith Reddy Vanga <akshithreddyvanga739@gmail.com>
2024-08-18 17:26:41 +0530

    Diff is slightly modified

--- Diff.java
+++ Diff.java
@@ -1,7 +1,8 @@
-public class Sum{
+public class Diff{
     public static void main(String[] args){
         int a = 50;
         int b =6;
         System.out.println(a-b);
+        System.out.println("It worked");
     }
 }
